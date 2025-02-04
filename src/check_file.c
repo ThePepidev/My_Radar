c21bcbfba51448cb02c801a28aba6f41c257dca0
ThePepidev <mathys1.dupont@epitech.eu>
2025-02-04 13:14:18 +0100

    feat(check_file.c): check all values

--- src/check_file.c
+++ src/check_file.c
@@ -8,6 +8,32 @@
 #include "my.h"
 #include "my_radar.h"
 
+void my_free_word_array(char **array)
+{
+    for (int i = 0; array[i] != NULL; i++)
+        free(array[i]);
+    free(array);
+}
+
+static int check_line_content(char *line)
+{
+    char **tokens = my_str_to_word_array(line, " ");
+    int token_count = 0;
+
+    for (int i = 0; tokens[i]; i++)
+        token_count++;
+    if (line[0] == 'A' && token_count != 7) {
+        my_free_word_array(tokens);
+        return 84;
+    }
+    if (line[0] == 'T' && token_count != 4) {
+        my_free_word_array(tokens);
+        return 84;
+    }
+    my_free_word_array(tokens);
+    return 0;
+}
+
 static int check_line(char *line)
 {
     if (!line)
@@ -22,7 +48,7 @@ static int check_line(char *line)
         if ((line[i] < '0' || line[i] > '9') && line[i] != '.')
             return 84;
     }
-    return 0;
+    return check_line_content(line);
 }
 
 int check_file(char **av)
@@ -38,6 +64,7 @@ int check_file(char **av)
     while (nread != -1) {
         if (check_line(line) == 84) {
             free(line);
+            fclose(fd);
             return 84;
         }
         nread = getline(&line, &len, fd);
